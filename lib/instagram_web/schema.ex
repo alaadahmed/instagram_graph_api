3fb7f19890091fa7c5237b66efa47ac88a6a821e
AlaaEldin Ahmed <alaadahmed@gmail.com>
2019-06-15 13:49:52 +0200

    implement query for photos and its resolver

--- lib/instagram_web/schema.ex
+++ lib/instagram_web/schema.ex
@@ -1,12 +1,19 @@
 defmodule InstagramWeb.Schema do
   use Absinthe.Schema
+  alias InstagramWeb.Resolvers
+
+  import_types(__MODULE__.Types)
 
   query do
+    @desc "Get a list of photos"
+    field :photos, list_of(:photo_type) do
+      resolve(&Resolvers.PhotoResolver.photos/3)
+    end
   end
 
   # mutation do
   # end
-  
+
   # subscription do
   # end
 end
